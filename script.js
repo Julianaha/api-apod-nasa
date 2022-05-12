e82fe68c9a4b75a82bd656c38a8518edbd4531f1
Juliana Souza <julianha01@gmail.com>
2022-05-12 17:19:35 -0300

    alteracoes de estilo

--- script.js
+++ script.js
@@ -9,20 +9,30 @@ $('#btn').click(function (e) {
         success: function (requisicao) {
             obj = requisicao
             selecionaTipo(obj)
+        },
+
+        error: function () {
+            msgErro()
         }
     })
 })
 
 function selecionaTipo(obj) {
+
     if (obj.media_type === 'image') {
         $('#img').attr('src', obj.url),
-            $('#img').css('display', 'inline'),
+            $('#img').css('display', 'flex'),
             $('#video').css('display', 'none')
     } else {
         $('#img').attr('src', obj.url),
             $('#img').css('display', 'none'),
             $('#video').attr('src', obj.url),
-            $('#video').css('display', 'inline')
+            $('#video').css('display', 'flex')
     }
 }
+
+function msgErro() {
+    $('#msg-erro').html('<p style="font-size:300%; text-align:center; color:#fb3d22">Veririfique a data selecionada e tente novamente!</p>');
+}
+
 
