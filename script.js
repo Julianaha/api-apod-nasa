f534d24426cfea721df03989979fa4569cb3a468
Juliana Souza <julianha01@gmail.com>
2022-05-14 23:30:32 -0300

    adicionando texto apos imagem

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let obj = {};
+const texto = $('.texto');
 
 $('#btn').click(function (e) {
     e.preventDefault();
@@ -7,12 +8,13 @@ $('#btn').click(function (e) {
         url: `https://api.nasa.gov/planetary/apod?api_key=d7nsRkq8FvACVzcgqGQjjfp5l1T6G1WhbNjfNuxG&date=${data}`,
         type: 'GET',
         success: function (requisicao) {
-            obj = requisicao
+            obj = requisicao;
             selecionaTipo(obj)
+            texto.text(obj.explanation)
         },
 
         error: function () {
-            msgErro()
+            $('#msg-erro').html('<p style="font-size:300%; text-align:center; color:#fb3d22">Veririfique a data selecionada e tente novamente!</p>');
         }
     })
 })
@@ -21,18 +23,14 @@ function selecionaTipo(obj) {
 
     if (obj.media_type === 'image') {
         $('#img').attr('src', obj.url),
-            $('#img').css('display', 'flex'),
-            $('#video').css('display', 'none')
+        $('#img').css('display', 'flex'),
+        $('#video').css('display', 'none')
     } else {
         $('#img').attr('src', obj.url),
-            $('#img').css('display', 'none'),
-            $('#video').attr('src', obj.url),
-            $('#video').css('display', 'flex')
+        $('#img').css('display', 'none'),
+        $('#video').attr('src', obj.url),
+        $('#video').css('display', 'flex')
     }
-}
-
-function msgErro() {
-    $('#msg-erro').html('<p style="font-size:300%; text-align:center; color:#fb3d22">Veririfique a data selecionada e tente novamente!</p>');
 }
 
 
